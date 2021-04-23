d497c7902dacffb9fd19330f9dbc0b04be90d44a
Oscar Cortes <olcortesb@gmail.com>
2021-04-23 11:51:51 -0300

    Agregar busqueda por año

--- omdbapp/app/controllers/movies_controller.rb
+++ omdbapp/app/controllers/movies_controller.rb
@@ -1,53 +1,73 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
-    def index
-      @movies = Movie.all
+  def index
+    @movies = Movie.all
+  end
+
+  def search
+    movies_local = Movie.where("title LIKE :query", query: params[:movie])
+
+    if params[:year]
+      movie_local = movies_local.find { |m| m.year == params[:year] }
     end
 
-    def search
-        movies = find_movies(params[:movie] || "")
+    movie_local = if movie_local then
+                    movie_local
+                  else
+                    movies_local.first
+                  end
 
-        if movies["Response"] == "False"
-            flash[:alert] = "Pelicula no encontrada"
-            return redirect_to root_path
-        end
+    unless movie_local
+      movies = find_movies(params[:movie] || "")
 
-        Movie.find_or_create_by(imdb_id: movies['imdbID']) do |m|
-          m.title = movies['Title']
-          m.year = movies['Year']
-          m.rate = movies['Rated']
-          m.released = movies['Released']
-          m.runtime = movies['Runtime']
-          m.director = movies['Director']
-          m.writer = movies['Writer']
-          m.plot = movies['Plot']
-          m.language = movies['Language']
-          m.country = movies['Country']
-          m.poster = movies['Poster']
-          m.imdb_id = movies['imdbID']
-          m.production = movies['Production']
-        end
+      if movies["Response"] == "False"
+        flash[:alert] = "Pelicula no encontrada"
+        return redirect_to root_path
+      end
 
-        @movie = movies
-        #@weather = find_weather(@country['capital'], @country['alpha2Code'])
+      @movie = save_movie(movies)
+      return
     end
 
-    private
-    def request_api(url)
-      response = Excon.get(
-        url,
-        headers: {
-          'X-RapidAPI-Host' => URI.parse(url).host
-        }
-      )
-      return nil if response.status != 200
-      JSON.parse(response.body)
-    end
+    @movie = movie_local
+  end
 
-    def find_movies(name)
-      request_api(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=9a044c5c&t=#{name}"
-      )
+  private
+
+  def save_movie(movies)
+    movie_save = Movie.find_or_create_by(imdb_id: movies['imdbID']) do |m|
+      m.title = movies['Title']
+      m.year = movies['Year']
+      m.rate = movies['Rated']
+      m.released = movies['Released']
+      m.runtime = movies['Runtime']
+      m.director = movies['Director']
+      m.writer = movies['Writer']
+      m.plot = movies['Plot']
+      m.language = movies['Language']
+      m.country = movies['Country']
+      m.poster = movies['Poster']
+      m.imdb_id = movies['imdbID']
+      m.production = movies['Production']
     end
+    return movie_save
+  end
+
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-RapidAPI-Host' => URI.parse(url).host
+      }
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body, object_class: OpenStruct)
+  end
+
+  def find_movies(name)
+    request_api(
+      "http://www.omdbapi.com/?i=tt3896198&apikey=9a044c5c&t=#{name}"
+    )
+  end
 end
