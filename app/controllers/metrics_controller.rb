0feb0606b47fdc0216aff8372b7b568c3a527f8a
Oscar Cortes <olcortesb@gmail.com>
2021-04-25 14:53:08 -0300

    refactor:metric controller

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
 class MetricsController < ApplicationController
+
   def home
     @latest = Movie.all.order(:year).take
     @recent = Movie.all.most_recent.take
     @count = Movie.all.count
-    @rating = Movie.where("imdb_rating > ?", 0).order(imdb_rating: :desc).take
+    @rating = Movie.where('imdb_rating > ?', 0).order(imdb_rating: :desc).take
     @year_average = Integer(Movie.average(:year))
-
-    all_movie = Movie.where("runtime != ?", 'N/A')
-    runtime_max = 0
-    avg = 0
-    all_movie.each { |m|
-      r = Integer(m.runtime.split(" ")[0])
-      avg = avg + r
-      if r > runtime_max
-        runtime_max = r
-        @runtime_max = m
-      end
-      @year_runtime = avg / @count
-    }
-
+    @runtime_max, @year_runtime = runtime_metrics
     @by_year = Movie.all.group_by(&:year)
-
   end
+
+  private
+
+  def runtime_metrics
+    all_movie = Movie.where('runtime != ?', 'N/A')
+    runtime_max = avg = 0
+    all_movie.each { |m|
+      r = Integer(m.runtime.split(' ')[0])
+      avg += r
+      runtime_max = r if r >= runtime_max
+      @runtime_max_m = m if r >= runtime_max
+      @year_runtime_avg = avg / @count
+    }
+    [@runtime_max_m, @year_runtime_avg]
+  end
+
 end
