5c9f237dc27de739f70bf37d8af4e6ea40b7eb55
Oscar Cortes <olcortesb@gmail.com>
2021-04-26 00:32:21 -0300

    refactor: agregando test a metrics y movies

--- spec/controllers/metrics_controller_spec.rb
+++ spec/controllers/metrics_controller_spec.rb
@@ -1,14 +1,59 @@
 require 'rails_helper'
 
-describe 'MetricsController' do
+RSpec.describe MetricsController , type: :controller do
   login_user
 
   before do
     @controller = MetricsController.new
   end
 
-  it 'OK :home' do
-    get :home
-    expect(response).to have_http_status(:ok)
+  describe 'GET #home' do
+    it 'Return a success response' do
+      get :home
+      expect(response).to have_http_status(:ok)
+    end
   end
+
+  describe 'GET #home Assigns' do
+    it 'Assigns to @latest value' do
+      get :home
+      expect(assigns[:latest]).to be_a(Movie)
+    end
+
+    it 'Assigns to @recent value' do
+      get :home
+      expect(assigns[:recent]).to be_a(Movie)
+    end
+
+    it 'Assigns to @rating value' do
+      get :home
+      expect(assigns[:rating]).to be_a(Movie)
+    end
+
+    it 'Assigns to @year_average value' do
+      get :home
+      expect(assigns[:year_average]).to be_a(Integer)
+    end
+
+    it 'Assigns to @runtime_max value' do
+      get :home
+      expect(assigns[:runtime_max]).to be_a(Movie)
+    end
+
+    it 'Assigns to @year_runtime value' do
+      get :home
+      expect(assigns[:year_runtime]).to be_a(Integer)
+    end
+
+  end
+
+  describe 'GET #home' do
+    it 'render the home metrics template' do
+      get :home
+      expect(response).to render_template :home
+    end
+  end
+
 end
+
+
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'MoviesController', type: :controller do
+RSpec.describe MoviesController, type: :controller do
   login_user
 
   before do
     @controller = MoviesController.new
   end
 
-  it 'OK :index' do
-    get :index
-    expect(response).to have_http_status(:ok)
+  describe 'GET #index' do
+    it 'Return a success response' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
   end
 
+  describe 'GET #home Assigns' do
+    it 'Assigns to @movies value' do
+      get :index
+      expect(assigns[:movies]).to be_a(ActiveRecord::Relation)
+    end
+  end
+
+  describe 'GET #index' do
+    it 'render the home index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
